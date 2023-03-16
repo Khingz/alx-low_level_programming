430754c451612204a2a56870c73bd19258531e55
Khingz <kingsley995mr@gmail.com>
2023-03-16 07:30:28 -0700

    added sizeof solution

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/** 
+ *main - Entry point
+ *
+ * description: print size of var
+ *
+ * return: Always (0) success
+*/
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+	printf("Size of char is: %i byte(s)", sizeof(a);
+	printf("Size of int is: %i byte(s)", sizeof(b);
+	printf("Size of long int is: %i byte(s)", sizeof(c);
+	printf("Size of long long int is: %i byte(s)", sizeof(d);
+	printf("Size of float is: %i byte(s)", sizeof(f);
+
+	return (0);
+}
