2e550fd60e70a7d57cac19ac5caa02ca32b00828
Khingz <kingsley995mr@gmail.com>
2023-03-16 07:41:58 -0700

    fix: type conversion to it

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,11 +15,11 @@ int main(void)
 	long long int d;
 	float f;
 
-	printf("Size of char is: %i byte(s)", sizeof(a);
-	printf("Size of int is: %i byte(s)", sizeof(b);
-	printf("Size of long int is: %i byte(s)", sizeof(c);
-	printf("Size of long long int is: %i byte(s)", sizeof(d);
-	printf("Size of float is: %i byte(s)", sizeof(f);
+	printf("Size of char is: %i byte(s)\n", (int) sizeof(a));
+	printf("Size of int is: %i byte(s)\n", (int) sizeof(b));
+	printf("Size of long int is: %i byte(s)\n", (int) sizeof(c));
+	printf("Size of long long int is: %i byte(s)\n", (int) sizeof(d));
+	printf("Size of float is: %i byte(s)\n", (int) sizeof(f));
 
 	return (0);
 }
