2b639ee6a9edffed6f41b7219967e6c43f1f838f
Khingz <kingsley995mr@gmail.com>
2023-03-16 06:55:18 -0700

    added printf soltulion

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *decription: prints to the console
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char[] = "with proper grammar, but the outcome is a piece of art,";
+	
+	printf("%c \n" char);
+
+	return (0);
+}
