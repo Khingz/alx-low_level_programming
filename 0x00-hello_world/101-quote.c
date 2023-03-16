b815027b799ad04785dbe3c0f997ea128d821fb4
Khingz <kingsley995mr@gmail.com>
2023-03-16 11:24:10 -0700

    ****

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+gi
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+	char error[];
+
+	msg 2> errorMsg;
+
+	printf("%s\n", errorMsg);
+	
+	return (1);
+}
