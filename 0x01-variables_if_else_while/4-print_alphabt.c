889c10d55f072b3ed870248b7e2424c4d93a0778
Khingz <kingsley995mr@gmail.com>
2023-03-17 11:24:26 -0700

    added print alphabet solution

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	
+	for (i = 97; i < 123; i++)
+	{
+		if (i == 113 || i == 101)
+		{
+			
+		}
+		else
+		{
+			putchar(i);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
