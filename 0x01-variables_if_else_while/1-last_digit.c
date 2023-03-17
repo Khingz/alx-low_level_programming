09547520a6fae6faab8b7e855d0e11da9a2b7a81
Khingz <kingsley995mr@gmail.com>
2023-03-17 09:07:51 -0700

    fix: fix long characters per line

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	last_digit = n%10;
+	last_digit = n % 10;
 
 	if (last_digit > 5 && n > 0)
 	{
@@ -27,13 +27,15 @@ int main(void)
 	{
 		printf("Last digit of %i is -%i and is greater than 5\n", n, last_digit);
 	}
-	else if (last_digit < 6 && last_digit > 0 && n > 0)
+	else if (last_digit < 6 && last_digit != 0 && n > 0)
 	{
-		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last_digit);
+		printf("Last digit of %i is %i and is less than 6 and not 0\n",
+		n, last_digit);
 	}
-	else if (last_digit < 6 && last_digit > 0 && n < 0)
+	else if (last_digit < 6 && last_digit != 0 && n < 0)
 	{
-		 printf("Last digit of %i is -%i and is less than 6 and not 0\n", n, last_digit);
+		 printf("Last digit of %i is -%i and is less than 6 and not 0\n",
+		 n, last_digit);
 	}
 	else if (last_digit == 0)
 	{
