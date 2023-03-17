481ed6a22ac45a8421ea0ca47faf1f2c7b24b095
Khingz <kingsley995mr@gmail.com>
2023-03-17 12:35:25 -0700

    added print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x;
+	int i;
+	
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+	}
+
+	for (x = 97; x <= 102; x++)
+	{
+		putchar(x);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
