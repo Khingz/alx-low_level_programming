895bee1b991ba68b29a834401c08425899cf314c
Khingz <kingsley995mr@gmail.com>
2023-03-17 09:13:45 -0700

    fix: removed paranthesis from else statement

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -37,7 +37,7 @@ int main(void)
 		 printf("Last digit of %i is %i and is less than 6 and not 0\n",
 		 n, last_digit);
 	}
-	else (last_digit == 0)
+	else
 	{
 		printf("Last digit of %i is %i and is 0\n", n, last_digit);
 	}
