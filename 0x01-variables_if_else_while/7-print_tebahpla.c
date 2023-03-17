7fa24f0bd3f12ae146299874826de1fd33d9682a
Khingz <kingsley995mr@gmail.com>
2023-03-17 12:20:21 -0700

    added mirror number

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x;
+	
+	for (x = 122; x >= 97; x--)
+	{
+		putchar(x);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
