808ad827c8744e433d1aca5b08c700a4c4156949
Khingz <kingsley995mr@gmail.com>
2023-03-18 09:22:04 -0700

    fix: added seperating characters to output

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -21,11 +21,13 @@ int main(void)
 					putchar(j + '0');
 					putchar(k + '0');
 
-					if ((i == 7 && j == 8) && k == 9)
+					if (i == 7 && j == 8 && k == 9)
 					{
-						putchar(44);
-						putchar(32);
+						break;
 					}
+
+					putchar(44);
+					putchar(32);
 				}
 			}
 		}
