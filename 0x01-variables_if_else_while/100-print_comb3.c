56f0afc31a6d0f9a871d9d4111fbe09db315de69
Khingz <kingsley995mr@gmail.com>
2023-03-18 08:30:16 -0700

    alog that displays unique 2 digits numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+	int comma_space[] = {44, 32};
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (i < j)
+			{
+				int x = 0;
+				putchar(i + '0');
+				putchar(j + '0');
+
+				while (x < 2)
+				{
+					putchar(comma_space[x]);
+					x++;
+				}
+			}
+		}
+	}
+	
+	putchar('\n');
+
+	return (0);
+}
