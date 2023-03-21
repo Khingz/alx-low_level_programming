fcc17fd372b02335d1eda9695cd8e9c66875cf3a
Khingz <kingsley995mr@gmail.com>
2023-03-21 04:03:45 -0700

    solving for 1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <main.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 97;
+
+	while (i > 123)
+	{
+		_putchar(i);
+	}
+	
+	_putchar('\n');
+
+	return (0);
+}
