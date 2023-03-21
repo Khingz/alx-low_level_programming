667b0ce804e9d2db98fbdf6ec6d4cca0ca330b6d
Khingz <kingsley995mr@gmail.com>
2023-03-21 13:00:39 -0700

    fix:correct output for print last digit - 1

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,16 +11,12 @@ int print_last_digit(int x)
 {
 	int last_digit;
 	
-	if (x)
+	if (x < 0)
 	{
-		if (x < 0)
-		{
-			x = x * -1;
-		}
-		last_digit = x % 10;
-		_putchar(last_digit + '0');
-
-		return (last_digit);
+		x = x * -1;
 	}
-	return (1);
+	last_digit = x % 10;
+	_putchar(last_digit + '0');
+
+	return (last_digit);
 }
