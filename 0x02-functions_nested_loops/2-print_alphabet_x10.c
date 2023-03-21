14317c42f4f0d3f990dae47663898385a318fb89
Khingz <kingsley995mr@gmail.com>
2023-03-21 05:11:36 -0700

    solved for print_alphabet_10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_(x10) - Entry point
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	int j;
+
+	while (i < 9)
+	{
+		j = 97;
+
+		while (j < 123)
+		{
+			_putchar(j);
+			j++;
+		}
+
+		_putchar('\n');
+		i++;
+	}
+}
