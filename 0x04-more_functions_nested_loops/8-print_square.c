524ebdfd05f4eab87d379460c784279aa5825dfb
Khingz <kingsley995mr@gmail.com>
2023-03-23 07:51:23 -0700

    added solution for square printing

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_square - Entry point
+ *
+ * @size: int for size of block
+ * Return: Always void
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size < 1)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		i = 0;
+		while (i < size)
+		{
+			j = 0;
+			while (j < size)
+			{
+				_putchar('#');
+				j++;
+			}
+			_putchar('\n');
+			i++;
+		}
+		_putchar('\n');
+	}
+}
