354eff08935a8371629195c45ac513d3793e1933
Khingz <kingsley995mr@gmail.com>
2023-03-23 03:10:24 -0700

    solved for mul

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * _isdigit - Entry point
+ *
+ * @c: integer to chec for
+ * Return: Always 0 (Success)
+ */
+int _isdigit(int c)
+{
+	if (c <= 48 && c >= 57)
+	{
+		return (1);
+	}
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * mul - Entry point
+ *
+ * @a: first operand
+ * @b: second operand
+ *
+ * Return: Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
