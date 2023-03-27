098688a79083521fc634ef81dd812813ff364f06
Khingz <kingsley995mr@gmail.com>
2023-03-27 09:19:58 -0700

    added print_rev solution

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,15 +7,39 @@
  *
  * Return: Always void
  */
+
+int strrlen(char *s);
+
 void print_rev(char *s)
 {
 	int len;
 	char *i;
 
-	len = _strlen(s);
+	len = strrlen(s);
 	for (i = s + len; i >= s; i--)
 	{
 		_putchar(*i);
 	}
 	_putchar('\n');
+}
+
+
+/**
+ * strrlen - check length string lenth
+ *
+ * @s: string to be checked
+ *
+ * Return: Always int (Success)
+ */
+
+int strrlen(char *s)
+{
+	int length;
+
+	length = 0;
+	for (; *s != '\0'; s++)
+	{
+		length++;
+	}
+	return (length);
 }
