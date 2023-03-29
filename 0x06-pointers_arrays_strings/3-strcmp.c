1251f0c38c5e3120d3cb89827b3eff9362e5ac28
Khingz <kingsley995mr@gmail.com>
2023-03-29 13:35:38 -0700

    added strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,29 @@
+/**
+ * _strcmp - Entry point
+ *
+ * @s1: string concat destination
+ * @s2:  source
+ *
+ * Return: Always 0 (Success)
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int check;
+	int i;
+
+	check = i = 0;
+	while (*s2 != '\0')
+	{
+		if (s1[i] - *s2 == 0)
+		{
+			s2++;
+			i++;
+		}
+		else
+		{
+			check = (s1[i] - s2[i]);
+			break;
+		}
+	}
+	return (check);
+}
