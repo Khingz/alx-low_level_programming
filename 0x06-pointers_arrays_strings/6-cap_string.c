0ddee87aa4fbcc0a86ceb732e478d00c6942a7b4
Khingz <kingsley995mr@gmail.com>
2023-03-30 13:00:21 -0700

    added cap solution

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,11 +8,27 @@
 char *cap_string(char *str)
 {
 	char *tmp;
+	int i;
+	char *sep = "\n\t .,(){}!?;";
 
 	tmp = str;
 	while (*tmp)
 	{
-		
+		if (*tmp == *str && *tmp >= 97 && *tmp <= 122)
+		{
+			*tmp -= 32;
+		}
+		i = 0;
+		while (i < 13)
+		{
+			if (*tmp == sep[i] && *(tmp + 1) >= 97 && *(tmp + 1) <= 122)
+			{
+				*(tmp + 1) -= 32;
+				break;
+			}
+			i++;
+		}
+		tmp++;
 	}
-	return (0);
+	return (str);
 }
