833e9c7deceda761440021360db0329d5570a1a3
Khingz <kingsley995mr@gmail.com>
2023-04-03 15:48:44 -0700

    solve ve for

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strchr - Entry point
+ *
+ * @s: string to be searched
+ * @c: character to be searched
+ * Return: Pointer to the first occurrence of the character in the string,
+ *         or NULL if the character is not found
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] >= '\0')
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+		i++;
+	}
+	return (0);
+}
