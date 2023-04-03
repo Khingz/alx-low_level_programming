8c2349c9ce0529c687dc3dcc06f3ada7709f2c25
Khingz <kingsley995mr@gmail.com>
2023-04-03 05:37:02 -0700

    added strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include <stddef.h>
+
+/**
+ * _strchr - Entry point
+ *
+ * @s: string to be searched
+ * @c: character to be searched
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
