3f9c707367d23e38e1e3e895801d0448e5b89524
Khingz <kingsley995mr@gmail.com>
2023-04-03 15:09:58 -0700

    try another method

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,10 +13,13 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	for (; s[i] != '\0'; i++)
+	while (i >= '\0')
 	{
 		if (s[i] == c)
-			return (&s[i]);
+		{
+			return (&(s[i]));
+		}
+		i++;
 	}
 	return (0);
 }
