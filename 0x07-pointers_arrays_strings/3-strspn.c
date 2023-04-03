5f5f935461506957107ec68ea00f9f18c2ae3d21
Khingz <kingsley995mr@gmail.com>
2023-04-03 16:23:03 -0700

    added strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,5 +8,27 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	return (0);
+	unsigned int len;
+	int i;
+
+	len = 0;
+	while (*s)
+	{
+		i = 0;
+		while (accept[i] >= '\0')
+		{
+			if (*s == accept[i])
+			{
+				len += 1;
+				break;
+			}
+			if (accept[i] == '\0')
+			{
+				return (len);
+			}
+			i++;
+		}
+		s++;
+	}
+	return (len);
 }
