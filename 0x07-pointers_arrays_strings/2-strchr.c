ad698879f5a49db21a9879e6b610ba9a8fc0a156
Khingz <kingsley995mr@gmail.com>
2023-04-03 14:18:33 -0700

    fix: switched to for loop

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,14 +10,15 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
-	{
-		if (*s == c)
-		{
-			return (s);
-		}
-		s++;
-	}
+	int i;
 
+	i = 0;
+	for (; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (&(s[i]));
+		}
+	}
 	return (0);
 }
