ac5a2086eb4c36ca1287c96d4c4325fe9cb84562
Khingz <kingsley995mr@gmail.com>
2023-04-06 17:41:17 -0700

    fix: checker 8, used long double

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,3 +1,5 @@
+int sqrt_helper(int n, int upper_bound);
+
 /**
  * _sqrt_recursion - Entry point
  *
@@ -5,7 +7,6 @@
  *
  * Return: Always 0 (Success)
  */
-int sqrt_helper(int n, int upper_bound);
 
 int _sqrt_recursion(int n)
 {
@@ -24,26 +25,26 @@ int _sqrt_recursion(int n)
  * sqrt_helper - check for square root
  *
  * @n: int to check square root
- * @upper_bound: upper bound to be checked
+ * @upper_bound: int upper bound to be checked
  *
  * Return: Always 0 (Success)
  */
 int sqrt_helper(int n, int upper_bound)
 {
-	unsigned int middle_number = upper_bound / 2;
-	unsigned int sqrt = middle_number * middle_number;
+	long double  middle_number = upper_bound / 2;
+	long double sqrt = middle_number * middle_number;
 
-	if (1 > upper_bound)
+	if (upper_bound < 2)
 	{
 		return (-1);
 	}
 
-	if (sqrt == (unsigned int) n)
+	if (sqrt ==  n)
 	{
 		return (middle_number);
 	}
 
-	if (sqrt > (unsigned int) n)
+	if (sqrt >  n)
 	{
 		return (sqrt_helper(n, upper_bound - 1));
 	}
