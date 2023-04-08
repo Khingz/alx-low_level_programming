74fe73268f6860384863b0f7d99b83ad10e96947
Khingz <kingsley995mr@gmail.com>
2023-04-08 09:40:07 -0700

    added solution 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: always zero
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 3)
+	{
+		printf("Error");
+		return (1);
+	}
+	printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
