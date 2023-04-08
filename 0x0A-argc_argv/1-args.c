7532de996927b6e515e3464d42e73fc4639dd52b
Khingz <kingsley995mr@gmail.com>
2023-04-08 09:26:09 -0700

    added solution 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: always zero
+ */
+
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%i\n", argc);
+	return (0);
+}
