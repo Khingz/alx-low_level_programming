420df11cca109a306c15791f11cacee7228cf6dd
Khingz <kingsley995mr@gmail.com>
2023-04-09 12:19:52 -0700

    fix: created an isdigit function to check validity of digit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,6 +2,8 @@
 #include <ctype.h>
 #include <stdlib.h>
 
+int _isdigit(char *s);
+
 /**
  * main - Entry point
  *
@@ -15,27 +17,48 @@ int main(int argc, char *argv[])
 {
 	int i, sum;
 
-	sum = 0;
 	if (argc < 2)
 	{
-		printf("%i\n", sum);
+		printf("0\n");
 		return (1);
 	}
 
 	i = 1;
+	sum = 0;
 	while (i < argc)
 	{
-		if (isdigit(*argv[i]))
-		{
-			sum += atoi(argv[i]);
-			i++;
-		}
-		else
+		if (_isdigit(argv[i]) == 1)
 		{
 			printf("Error\n");
 			return (1);
 		}
+		sum += atoi(argv[i]);
+		i++;
 	}
 	printf("%i\n", sum);
+	return (0);
+}
+
+/**
+ * _isdigit - checks if a string is a number
+ *
+ * @s: string to be checked
+ *
+ * Return: 0 or 1;
+ */
+
+int _isdigit(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (!isdigit(s[i]))
+		{
+			return (1);
+		}
+		i++;
+	}
 	return (0);
 }
