0bfe575b5fb2d2243b0524ffe003d34405d0c0c0
Khingz <kingsley995mr@gmail.com>
2023-04-11 03:58:10 -0700

    fix: checker 3

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -28,13 +28,13 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 	}
 	i = 0;
-	while (s1[i] != '\0')
+	while (s1 != NULL && s1[i] != '\0')
 	{
 		tmp[i] = s1[i];
 		i++;
 	}
 	j = 0;
-	while (s2[j] != '\0')
+	while (s2 != NULL && s2[j] != '\0')
 	{
 		tmp[i] = s2[j];
 		i++;
