0507a18f2f53300bfa883eccaafd07f2ac952d99
Khingz <kingsley995mr@gmail.com>
2023-04-13 13:33:50 -0700

    fix: null character

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,4 +1,5 @@
 #include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -12,36 +13,41 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *buffer;
-	unsigned int len, i, j, len_s1;
+	unsigned int len_s1, len_s2, len_buffer, j;
 
 	if (s1 == NULL)
 	{
-		*s1 = '\0';
+		s1 = "";
 	}
 	if (s2 == NULL)
 	{
-		*s2 = '\0';
+		s2 = "";
 	}
 	len_s1 = strlen(s1);
-	len = len_s1 + n + 1;
-	buffer = malloc(sizeof(char) * len);
+	len_s2 = strlen(s2);
+	if (n > len_s2)
+	{
+		n = len_s2;
+	}
+	len_buffer = len_s1 + n;
+	buffer = malloc(len_buffer + 1);
 	if (buffer == NULL)
 	{
 		return (NULL);
 	}
-	i = 0;
-	while (s1[i] != '\0')
-	{
-		buffer[i] = s1[i];
-		i++;
-	}
 	j = 0;
-	while (j < n)
+	while (j < len_buffer)
 	{
-		buffer[i] = s2[j];
-		i++;
+		if (j < len_s1)
+		{
+			buffer[j] = s1[j];
+		}
+		else
+		{
+			buffer[j] = s2[j - len_s1];
+		}
 		j++;
 	}
-	buffer[i] = '\0';
+	buffer[j] = '\0';
 	return (buffer);
 }
