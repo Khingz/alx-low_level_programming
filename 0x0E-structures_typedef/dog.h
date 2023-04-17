4c88015b549c591b59b8ad0b523ffcaa01361cb5
Khingz <kingsley995mr@gmail.com>
2023-04-17 05:19:53 -0700

    fix: created typedef on a seperste lime

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,13 +9,18 @@
 * @owner: Third member
 * Description: Longer description
 */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 }
-dog_t;
+dog_t
+
+/**
+ * dog_t - typedef for struct
+ */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
