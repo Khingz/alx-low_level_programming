284766edbb4827ec4d967c85e11676f40d067e5c
Khingz <kingsley995mr@gmail.com>
2023-04-17 12:43:39 -0700

    fix: implemented strcpy

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 #include <stddef.h>
 #include "dog.h"
 
@@ -12,14 +13,34 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *_dog;
+	int strlen_name, strlen_owner;
+
+	strlen_name = strlen(name);
+	strlen_owner = strlen(owner);
 
 	_dog = malloc(sizeof(dog_t));
 	if (_dog == NULL)
 	{
 		return (NULL);
 	}
-	_dog->name = name;
+
+	_dog->name = malloc(sizeof(char) * strlen_name + 1);
+	if (_dog->name == NULL)
+	{
+		free(_dog);
+		return (NULL);
+	}
+	_dog->owner = malloc(sizeof(char) * strlen_owner + 1);
+	if (_dog->owner == NULL)
+	{
+		free(_dog->name);
+		free(_dog);
+		return (NULL);
+	}
+	strcpy(_dog->name, name);
+	_dog->name[strlen_name + 1] = '\0';
+	strcpy(_dog->owner, owner);
+	_dog->owner[strlen_owner + 1] = '\0';
 	_dog->age = age;
-	_dog->owner = owner;
 	return (_dog);
 }
