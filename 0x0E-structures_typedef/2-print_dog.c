f72670fcd86c974693a1432f0d6cb1c12a6acf4e
Khingz <kingsley995mr@gmail.com>
2023-04-17 06:19:27 -0700

    fix: NULL for missing struct fields

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,6 +7,14 @@
  */
 void print_dog(struct dog *d)
 {
+	if (d->name == NULL)
+	{
+		d->name = "(nil)";
+	}
+	if (d->owner == NULL)
+        {
+                d->owner = "(nil)";
+        }
 	if (d != NULL)
 	{
 		printf("Name: %s\n", d->name);
