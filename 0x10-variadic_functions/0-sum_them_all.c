60147d56889f34d00646bb73aaed029c2a636765
Khingz <kingsley995mr@gmail.com>
2023-04-20 00:44:11 -0700

    added task 0 before checker release

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include <stdarg.h>
+/**
+ * sum_them_all - sum integers
+ * @n: number of ards
+ * Return: integer sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum;
+	unsigned int i;
+	va_list numbers;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	va_start(numbers, n);
+	sum = i = 0;
+	while (i < n)
+	{
+		sum += va_arg(numbers, int);
+		i++;
+	}
+	return (sum);
+}
+
