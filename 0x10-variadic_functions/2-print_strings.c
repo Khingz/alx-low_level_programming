fef4e4e53a971ca7ce7ce436d31b9bf8b881837d
Khingz <kingsley995mr@gmail.com>
2023-04-20 04:33:27 -0700

    fix: (nil) not (nill)

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		s = va_arg(str, char *);
 		if (s == NULL)
 		{
-			printf("(nill)\n");
+			printf("(nil)\n");
 			exit(1);
 		}
 		i++;
