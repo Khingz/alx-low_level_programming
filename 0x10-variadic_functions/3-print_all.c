69277d3b2f29d6472fb455294e57112a08412dd3
Khingz <kingsley995mr@gmail.com>
2023-04-21 12:20:51 -0700

    aded soluttion 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,10 +1,53 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_all - print according to specifier
+ * @format: speifier
+ * Return: Always void
+ */
+
 void print_all(const char * const format, ...)
 {
-	data_t spr[] = {
-		{"c", char},
-		{"i", int},
-		{"f", float},
-		{"s", char *}
-		{NULL, NULL}
-	};
+	va_list var;
+	int i;
+	char *w_sep, *tmp_str;
+
+	va_start(var, format);
+	w_sep = "";
+	i = 0;
+	if (format)
+	{
+		while (format[i] != '\0')
+		{
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", w_sep, va_arg(var, int));
+					break;
+				case 'f':
+					printf("%s%f", w_sep, va_arg(var, double));
+					break;
+				case 'i':
+					printf("%s%i", w_sep, va_arg(var, int));
+					break;
+				case 's':
+					tmp_str = va_arg(var, char*);
+					if (!tmp_str)
+					{
+						tmp_str = "(nil)";
+					}
+					printf("%s%s", w_sep, tmp_str);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			w_sep = ", ";
+			i++;
+		}
+	}
+	va_end(var);
+	printf("\n");
 }
