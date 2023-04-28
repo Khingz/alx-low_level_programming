ff2e169f1981f8a633c22bbdc29093dc9094bcdc
Khingz <kingsley995mr@gmail.com>
2023-04-28 12:20:17 -0700

    added solution 2

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * listint_t - add a node to the begining of a list
+ * @head: head of node
+ * @n: value of node->n
+ * Return: pointer to note
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_el;
+
+	new_el = malloc(sizeof(listint_t));
+	if (new_el == NULL)
+	{
+		return (NULL);
+	}
+	new_el->n = n;
+	new_el->next = *head;
+	*head = new_el;
+	return (new_el);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -17,9 +17,11 @@ typedef struct listint_s
 /* header files */
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /* function prototypes */
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
 
 #endif
