881f81c6eb7e283022f2a6c692d343dfd691252b
Khingz <kingsley995mr@gmail.com>
2023-04-28 07:58:05 -0700

    task 101 added

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,16 @@
+section .text
+	global main
+main:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, message
+	mov rdx, msg_len
+	syscall
+
+	mov rax, 60
+	mov rdi, 0
+	syscall
+
+section .data
+	message db "Hello Holberton", 10;
+	msg_len equ $ -message
