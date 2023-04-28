b6736ba1dfd0ba6364d2df65f7e97cef7efce6ce
Khingz <kingsley995mr@gmail.com>
2023-04-28 12:33:34 -0700

    added ssolution 3

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * add_noteint_end - add a node to the end of a string
+ * @head: head of list
+ * @n: int to add
+ * Return: pointer to new list
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *tmp, *new_el;
+
+	new_el = malloc(sizeof(listint_t));
+	if (new_el == NULL)
+	{
+		return (NULL);
+	}
+	new_el->n = n;
+	new_el->next = NULL;
+	if (*head == NULL)
+	{
+		*head =new_el;
+		return (new_el);
+	}
+	tmp = *head;
+	while (tmp->next != NULL)
+	{
+		tmp = tmp->next;
+	}
+	tmp->next = new_el;
+	return (new_el);
+}
