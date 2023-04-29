a61768884b190339c0f77630c5555e65b6346767
Khingz <kingsley995mr@gmail.com>
2023-04-29 06:55:41 -0700

    checker 5 failing

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,17 +9,19 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t count;
+	const listint_t *tmp;
 
+	tmp = h;
 	count = 0;
 	if (h == NULL)
 	{
 		return (count);
 	}
-	while (h != NULL)
+	while (tmp != NULL)
 	{
-		printf("%d\n", h->n);
+		printf("%d\n", tmp->n);
 		count++;
-		h = h->next;
+		tmp = tmp->next;
 	}
 	return (count);
 }
