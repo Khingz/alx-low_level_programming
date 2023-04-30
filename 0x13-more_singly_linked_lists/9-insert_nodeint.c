45b6d6912c56da528d0fd95c29b87472b4df69a3
Khingz <kingsley995mr@gmail.com>
2023-04-30 09:30:04 -0700

    *head shouldnt return NULL when NULL

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,7 +12,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int indx;
 	listint_t *new_el, *tmp_head;
 
-	if (head == NULL || *head == NULL)
+	if (head == NULL)
 	{
 		return (NULL);
 	}
