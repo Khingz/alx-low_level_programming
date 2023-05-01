036615e5e654996eb9c380df02932c21fc941b7d
Khingz <kingsley995mr@gmail.com>
2023-05-01 03:18:31 -0700

    resolving checker failing

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -8,7 +8,7 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *tmp, *prev_node, *temp_head;
+	listint_t *temp_head, *tmp, *del;
 	unsigned int idx;
 	int num;
 
@@ -30,14 +30,11 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		if (idx == (index - 1))
 		{
-			prev_node = temp_head;
-		}
-		if (idx == index)
-		{
-			tmp = temp_head->next;
-			num = temp_head->n;
-			free(temp_head);
-			prev_node->next = tmp;
+			del = temp_head->next;
+			tmp = del->next;
+			num = del->n;
+			free(temp_head->next);
+			temp_head->next = tmp;
 			return (num);
 		}
 		idx++;
