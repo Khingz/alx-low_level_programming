5513fb51e0febbefda38f467c7f5f721ae68f826
Khingz <kingsley995mr@gmail.com>
2023-05-08 07:47:54 -0700

    fix: line over 80 chars

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * read_textfile -  reads a text file and prints it to the POSIX standard output
+ * read_textfile - reads a text file and prints it to the POSIX std out
  * @filename: name= if file to read
  * @letters: size of char to read
  * Return: -1 or size read
