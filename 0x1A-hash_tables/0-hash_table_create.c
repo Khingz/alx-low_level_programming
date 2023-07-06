dc54ea3c7109a35c8a0dea7644900cbf5f5fa733
Khingz <kingsley995mr@gmail.com>
2023-07-06 13:17:35 -0700

    implemeted create hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a hash table
+ * @size: size of the table
+ * Return: pointer to table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *h_table;
+	unsigned long int n;
+
+	if (!size || size == 0)
+		return (NULL);
+
+	h_table = malloc(sizeof(hash_table_t));
+	if (h_table == NULL)
+		return (NULL);
+
+	h_table->size = size;
+	h_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (h_table->array == NULL)
+		return (NULL);
+
+	for (n = 0; n < size; n++)
+		h_table->array[n] = NULL;
+
+	return (h_table);
+}
