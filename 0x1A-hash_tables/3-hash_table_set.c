a8e86588104f617a30c155f038740ce905adb5d7
Khingz <kingsley995mr@gmail.com>
2023-07-07 05:48:55 -0700

    checked for null

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index, i;
 	hash_node_t *new;
 
-	if (!ht || !key || !value || *key == '\0')
+	if (ht == NULL || key == NULL || value == NULL || *key == '\0')
 		return (0);
 
 	copy_val = strdup(value);
