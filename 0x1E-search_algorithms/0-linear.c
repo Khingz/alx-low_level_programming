42711be593abafa19a0db7a5f3600203e3cce297
Khingz <kingsley995mr@gmail.com>
2023-10-09 01:57:10 -0700

    initial commit, task 0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,28 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - performs linear search alogrithm
+ * @array: array to search
+ * @size: number of array
+ * @value: value to search
+ * Return: 0 or 1
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (!array)
+		return (-1);
+
+	i = 0;
+	while (i < size)
+	{
+		if (array[i] == value)
+		{
+			return (i);
+		}
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		i++;
+	}
+	return (-1);
+}
